271aabc4bbd826d44d926461465febf950d6c9f4
Jack Ellis <jackellis1504@gmail.com>
2024-04-25 12:33:14 +0100

    printing functions and first vs nth preferences

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fmt, fs};
 
 const HEADER_ROWS: usize = 1;
 const NAME_INDEX: usize = 1;
@@ -6,6 +6,7 @@ const WING_INDEX: usize = 2;
 const LINK_INDEX: usize = 3;
 const MIDDLE_INDEX: usize = 4;
 const POSITIONS_CSV: &str = "./positions.csv";
+const FIRST_CHOICES_ONLY: bool = true;
 
 #[derive(Debug, Clone)]
 struct Player {
@@ -22,6 +23,16 @@ enum Position {
     Middle,
 }
 
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        return match self {
+            Position::Wing => write!(f, "Wing"),
+            Position::Link => write!(f, "Link"),
+            Position::Middle => write!(f, "Middle"),
+        }
+    }
+}
+
 fn main() {
     let val: String = match fs::read_to_string(POSITIONS_CSV) {
         Ok(s) => s,
@@ -35,33 +46,38 @@ fn main() {
             cells.push(player_from_row(row));
         }
     }
-    let wings: Vec<Player> = sort_by_position(cells.clone(), Position::Wing);
-    let links: Vec<Player> = sort_by_position(cells.clone(), Position::Link);
-    let middles: Vec<Player> = sort_by_position(cells.clone(), Position::Middle);
+    let wings: Vec<Player> = sort_by_position(cells.clone(), Position::Wing, FIRST_CHOICES_ONLY);
+    let links: Vec<Player> = sort_by_position(cells.clone(), Position::Link, FIRST_CHOICES_ONLY);
+    let middles: Vec<Player> = sort_by_position(cells.clone(), Position::Middle, FIRST_CHOICES_ONLY);
 
-    println!("WINGS");
-    for player in wings {
-        println!("\t{} - {}", player.name, player.wing)
-    }
-    println!("LINKS");
-    for player in links {
-        println!("\t{} - {}", player.name, player.link)
-    }
-    println!("MIDDLES");
-    for player in middles {
-        println!("\t{} - {}", player.name, player.middle)
+    print_players_in_position(wings, Position::Wing);
+    print_players_in_position(links, Position::Link);
+    print_players_in_position(middles, Position::Middle);
+}
+
+fn print_players_in_position(players: Vec<Player>, position: Position) -> () {
+    println!("{}", position);
+    for player in players {
+        let position_preference = match position {
+            Position::Wing => player.wing,
+            Position::Link => player.link,
+            Position::Middle => player.middle,
+        };
+        println!("\t{} - {}", player.name, position_preference)
     }
 }
 
-fn sort_by_position(input: Vec<Player>, position: Position) -> Vec<Player> {
+fn sort_by_position(input: Vec<Player>, position: Position, only_first_choices: bool) -> Vec<Player> {
     let get_value: &dyn Fn(&Player) -> u32 = &|p: &Player| match position {
         Position::Wing => p.wing,
         Position::Link => p.link,
         Position::Middle => p.middle,
     };
+
+    let max_preference = if only_first_choices { 2 } else { u32::MAX };
     let mut intermediary: Vec<Player> = input
         .into_iter()
-        .filter(|p: &Player| get_value(p) < u32::MAX)
+        .filter(|p: &Player| get_value(p) < max_preference)
         .collect();
     intermediary.sort_by(|p1: &Player, p2: &Player| get_value(p1).cmp(&get_value(p2)));
     return intermediary;
